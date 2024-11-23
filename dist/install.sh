e229323d1ad8454b51fba36bedeb2802449374e6
narumij <narumij@gmail.com>
2024-11-23 22:17:15 +0900

    install

--- dist/install.sh
+++ dist/install.sh
@@ -150,6 +150,6 @@ let package = Package(
 EOF
 ## install library
 #swift build -Xswiftc -O -Xlinker -lm -c release --swift-sdk x86_64-swift-linux-musl
-swift resolve
+swift packge resolve
 swift build -c release --swift-sdk x86_64-swift-linux-musl
 rm .build/release/Main
