c24760ab9e07724b7c39aa2d5f199cf0d35a8453
narumij <narumij@gmail.com>
2024-11-23 21:45:00 +0900

    test

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+cat << 'EOF' > Sources/main.swift
+import Foundation
+
+#if os(macOS) || os(iOS)
+    print("Hello, Apple platform!")
+#elseif canImport(Glibc)
+    print("Hello, Glibc!")
+#elseif canImport(Musl)
+    print("Hello, Musl!")
+#endif
+EOF
+
 export PATH=/usr/local/swift/usr/bin:$PATH
-swift build -Xswiftc -O -Xlinker -lm -c release 1>&2
+swift build -Xswiftc -O -Xlinker -lm -c release Main 1>&2
 
 swift run --configuration release Main
