854e19c82f0f5256055449987d77f6512f46c88c
narumij <narumij@gmail.com>
2024-11-23 22:55:17 +0900

    test

--- test/test.sh
+++ test/test.sh
@@ -14,8 +14,8 @@ import Foundation
 #endif
 EOF
 
-export PATH=/usr/local/swift/usr/bin:$PATH
-./usr/bin/swift build -c release --swift-sdk x86_64-swift-linux-musl 1>&2
+# export PATH=/usr/local/swift/usr/bin:$PATH
+./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift build -c release --swift-sdk x86_64-swift-linux-musl 1>&2
 
 ./.build/release/Main
 
