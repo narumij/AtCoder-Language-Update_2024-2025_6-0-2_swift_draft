6c492cec542b8ef4aee689153e1d84a1f2fbd6a5
narumij <narumij@gmail.com>
2024-11-24 12:11:38 +0900

    install

--- dist/install.sh
+++ dist/install.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
+PLATFORM=ubuntu24.04
+LANG_VERSION=6.0.2
+SDK_VERSION=0.0.1
+
+SWIFT_BRANCH=swift-${LANG_VERSION}-release
+SWIFT_VERSION=swift-${LANG_VERSION}-RELEASE
+SWIFT_WEBROOT=https://download.swift.org
+SWIFT_WEBDIR="$SWIFT_WEBROOT/$SWIFT_BRANCH/$(echo $PLATFORM | tr -d .)$OS_ARCH_SUFFIX"
+OS_ARCH_SUFFIX="" # arm64等の場合に指定する
+FILE_NAME="$SWIFT_VERSION-$PLATFORM$OS_ARCH_SUFFIX"
+SWIFT_TAR_BALL_URL="$SWIFT_WEBDIR/$SWIFT_VERSION/$FILE_NAME.tar.gz"
+SWIFT_TAR_BALL_FILE="$FILE_NAME.tar.gz"
+
+STATIC_LINUX_SDK_URL=https://download.swift.org/swift-${LANG_VERSION}-release/static-sdk/swift-${LANG_VERSION}-RELEASE/swift-${LANG_VERSION}-RELEASE_static-linux-${SDK_VERSION}.artifactbundle.tar.gz
+STATIC_LINUX_SDK_CHECKSUM=aa5515476a403797223fc2aad4ca0c3bf83995d5427fb297cab1d93c68cee075
+SWIFT=./${$FILE_NAME}/usr/bin/swift
+
 export DEBIAN_FRONTEND=noninteractive
 
 # このスクリプトでは、まず言語環境を構築し、その後ビルド環境を構築します。
 
 # ここから、言語環境の構築を開始します。
 # 言語環境の構築では、Swift toolchainの展開と、Static Linux SDKのインストールを行います。
-# 構築手順は、Swift公式サイトのGETTING STARTのINSTALL SWIFTに従います。
+# Swift toolchainの展開手順は、Swift公式サイトのGETTING STARTのINSTALL SWIFTに従います。
 
 # Linuxへのインストールの公式情報は以下です。
 # https://www.swift.org/install/linux/tarball/
@@ -33,7 +50,7 @@ sudo apt-get install -y \
 
 # 公式 2. Download the latest binary release (6.0.2).
 curl -s -O \
-  https://download.swift.org/swift-6.0.2-release/ubuntu2404/swift-6.0.2-RELEASE/swift-6.0.2-RELEASE-ubuntu24.04.tar.gz
+  $SWIFT_TAR_BALL_URL
 
 # 公式 3. Import and verify the PGP signature:
 # $ gpg --keyserver hkp://keyserver.ubuntu.com \
@@ -43,16 +60,16 @@ curl -s -O \
 # この手順は省略します
 
 # 公式 4. Extract the archive with the following command:
-tar xzf swift-6.0.2-RELEASE-ubuntu24.04.tar.gz
+tar xzf $SWIFT_TAR_BALL_FILE
 
 # 公式 5. Add the Swift toolchain to your path as follows:
 # $ export PATH=/path/to/usr/bin:"${PATH}"
-# 前回のやり取りの中で何か問題となっていたようなので、いったん行いません
+# 他の言語環境への影響が不明な為、省略します。
 
 # 公式のインストール手順は以上です
 
 # インストール結果を確認します
-./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift --version
+$SWIFT --version
 
 # Static Linux SDKの公式情報は、以下です
 # https://www.swift.org/documentation/articles/static-linux-getting-started.html
@@ -60,26 +77,26 @@ tar xzf swift-6.0.2-RELEASE-ubuntu24.04.tar.gz
 # Once that is out of the way, actually installing the Static Linux SDK is easy; at a prompt, enter
 # の部分です
 
-./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift \
+$SWIFT \
  sdk install \
- https://download.swift.org/swift-6.0.2-release/static-sdk/swift-6.0.2-RELEASE/swift-6.0.2-RELEASE_static-linux-0.0.1.artifactbundle.tar.gz \
-  --checksum aa5515476a403797223fc2aad4ca0c3bf83995d5427fb297cab1d93c68cee075
+ $STATIC_LINUX_SDK_URL
+  --checksum $STATIC_LINUX_SDK_CHECKSUM
 
 # Swift will download and install the SDK on your system. You can get a list of installed SDKs with
 # の部分です
 # SDKのインストール結果を確認します
-./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift sdk list
+$SWIFT sdk list
 
 # SDKのインストールは以上です
 # AtCoderからの要請で不要なファイルを削除するよう指示があるため、ダウンロードしたファイルを削除します
-rm swift-6.0.2-RELEASE-ubuntu24.04.tar.gz
+rm $SWIFT_TAR_BALL_FILE
 # これで言語環境の構築は完了しました
 
 # 続いて、コンパイル環境の構築を行います
-# コンパイル環境の構築では、AtCoderで使用するSwiftパッケージの初期化と事前ビルドを行います
+# コンパイル環境の構築では、AtCoderで使用するSwiftパッケージの初期化と依存パッケージの追加、そして事前ビルドを行います
 
 # ジャッジがビルドを行う作業パッケージの初期化を行います。パッケージ名はMain、実行可能なプログラムとして初期化します
-./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift package init --name Main --type executable
+$SWIFT package init --name Main --type executable
 
 # Package.swiftを更新し、AtCoderで使用する依存パッケージを作業パッケージに追加します
 cat << 'EOF' > Package.swift
@@ -148,12 +165,12 @@ let package = Package(
 EOF
 
 # 念の為に、クリーニングします
-./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift package clean
+$SWIFT package clean
 
 # 依存パッケージの解決とパッケージのビルドを事前に行います
-./swift-6.0.2-RELEASE-ubuntu24.04/usr/bin/swift build -c release --swift-sdk x86_64-swift-linux-musl
+$SWIFT build -c release --swift-sdk x86_64-swift-linux-musl
 
-# ビルド判定が正しく行われるよう、ビルド結果を削除します
+# ジャッジによるビルド判定が正しく行われるよう、ビルド結果を削除します
 # 標準的なパスは、.build/release/Mainですが、
 # .build/release/が、Static Linux SDKの影響で、
 # .build/x86_64-swift-linux-musl/release/へのシンボリックリンクとなっています
