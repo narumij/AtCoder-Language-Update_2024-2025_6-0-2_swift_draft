835c9f496324807a9aa1208bd97a03c481e56c7b
narumij <narumij@gmail.com>
2024-12-13 21:43:50 +0900

    test

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 cp test/main.swift Sources/main.swift
 
-tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//"              
+tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//" | sh              
 
 cat << 'EOF' | .build/release/Main
 3
@@ -13,14 +13,14 @@ cp test/abc235_d/main.1.swift Sources/main.swift
 
 # ./${SWIFT_TAR_BALL}/usr/bin/swift package clean
 
-tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//"              
+tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//" | sh              
 
 time .build/release/Main < test/abc235_d/sample-x.in
 
 cp test/abc235_d/main.2.swift Sources/main.swift
 # ./${SWIFT_TAR_BALL}/usr/bin/swift package clean
 
-tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//"              
+tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//" | sh              
 
 .build/release/Main < test/abc235_d/sample-x.in
 time .build/release/Main < test/abc235_d/sample-x.in
