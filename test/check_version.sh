7aea2897d5317d58a7c06411107c09a4e6550362
narumij <narumij@gmail.com>
2024-12-13 22:25:31 +0900

    version

--- test/check_version.sh
+++ test/check_version.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+PLATFORM=ubuntu24.04
+LANG_VERSION=6.0.3
+OS_ARCH_SUFFIX="" # arm64等の場合に指定する
+
+SWIFT_VERSION=swift-${LANG_VERSION}-RELEASE
+SWIFT_TAR_BALL="$SWIFT_VERSION-$PLATFORM$OS_ARCH_SUFFIX"
+
 # TOMLファイルとキーを指定
 TOML_FILE="dist/swift.toml"
 TOML_KEY="version"
@@ -7,8 +14,8 @@ TOML_KEY="version"
 # TOMLファイルからバージョンを取得
 TOML_VERSION=$(tq "$TOML_KEY" --file "$TOML_FILE" | sed -e "s/^'//" -e "s/'$//")
 
-# インストール済みのSwiftバージョンを取得
-INSTALLED_VERSION=$(swift --version | awk '{print $4}')
+# インストール済みのSwiftバージョンを取得（正確に抽出）
+INSTALLED_VERSION=$(./${SWIFT_TAR_BALL}/usr/bin/swift --version | awk '/Swift version/ {print $3}')
 
 # バージョンの比較
 if [ "$TOML_VERSION" = "$INSTALLED_VERSION" ]; then
