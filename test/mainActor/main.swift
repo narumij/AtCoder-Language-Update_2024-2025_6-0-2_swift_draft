7b1d93366f35ddf5fbabdac1722c00d3b7fce721
narumij <narumij@gmail.com>
2024-12-21 02:43:54 +0900

    memo

--- test/mainActor/main.swift
+++ test/mainActor/main.swift
@@ -2,7 +2,9 @@ import AcFoundation
 import Foundation
 
 private let main: () = {
-  do { try Answer() } catch { /* WA */  }
+    // 5.8.1では、nonisoletedなmain関数からMainActorを呼び出せなかったが、
+    // 6.0.3では呼び出せるようになっている
+     do { try Answer() } catch { /* WA */  }
 }()
 
 @MainActor
