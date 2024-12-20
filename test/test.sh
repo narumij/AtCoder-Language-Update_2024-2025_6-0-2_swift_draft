c053d08781ee716bf0f5191a1dba2fb7f6413759
narumij <narumij@gmail.com>
2024-12-21 01:23:44 +0900

    stack, heap

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Heap size: $(ulimit -a | grep 'max locked memory' | awk '{print $4}')"
+echo "Stack size: $(ulimit -a | grep 'stack size' | awk '{print $4}')"
 
 cp test/main.swift Sources/main.swift
 
