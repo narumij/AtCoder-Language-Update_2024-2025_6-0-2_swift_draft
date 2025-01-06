76ecab6f053543a86ea6cab226f177afedc8b06f
narumij <narumij@gmail.com>
2025-01-06 20:27:53 +0900

    try macro

--- test/macro/main.swift
+++ test/macro/main.swift
@@ -1,7 +1,4 @@
-import AcFoundation
 import Foundation
-import SortedCollections
-import Algorithms
 import swift_ac_memoize
 
 
@@ -12,14 +9,12 @@ private let main: () = {
 @MainActor
 @inlinable
 public func Answer() throws {
-  try MainActor.assumeIsolated {
-    try Macro0()
-  }
+  print("Hello, Swift Macros")
+  try Macro0()
 }
 
 @MainActor
 public func Macro0() throws {
-  print("Hello, Swift Macros")
   let (a,b) = (2,3)
   let (result, code) = #stringify(a + b)
   print("The value \(result) was produced by the code \"\(code)\"")
