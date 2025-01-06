135154368deec02a3813b69d76812655613e22a7
narumij <narumij@gmail.com>
2025-01-06 20:37:07 +0900

    try macro

--- test/macro/main.swift
+++ test/macro/main.swift
@@ -10,13 +10,7 @@ private let main: () = {
 @inlinable
 public func Answer() throws {
   print("Hello, Swift Macros")
-  try Macro0()
-}
-
-@MainActor
-public func Macro0() throws {
   let (a,b) = (2,3)
   let (result, code) = #stringify(a + b)
   print("The value \(result) was produced by the code \"\(code)\"")
-
 }
