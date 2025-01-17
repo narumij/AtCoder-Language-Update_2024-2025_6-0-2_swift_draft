f921ebf31baa47aa5e00d1be86c150d7eb2b984b
narumij <narumij@gmail.com>
2025-01-18 03:51:51 +0900

    stderr

--- test/stdio/main.swift
+++ test/stdio/main.swift
@@ -1,8 +1,7 @@
+import Foundation
 import AcFoundation
 
 private let main: () = {
-    // 5.8.1では、nonisoletedなmain関数からMainActorを呼び出せなかったが、
-    // 6.0.3では呼び出せるようになっている
      do { try Answer() } catch { /* WA */  }
 }()
 
