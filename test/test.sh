e435c2989a2ab8479f23511d285e192599294d88
narumij <narumij@gmail.com>
2025-02-03 04:28:03 +0900

    test

--- test/test.sh
+++ test/test.sh
@@ -73,3 +73,19 @@ tq 'compile' --file dist/swift.toml | sed -e "1s/^'''//" -e "\$s/'''$//" | bash
 cat << 'EOF' | .build/release/Main
 EOF
 
+PLATFORM=ubuntu24.04
+LANG_VERSION=6.0.3
+OS_ARCH_SUFFIX="" # arm64等の場合に指定する
+
+SWIFT_VERSION=swift-${LANG_VERSION}-RELEASE
+SWIFT_TAR_BALL="$SWIFT_VERSION-$PLATFORM$OS_ARCH_SUFFIX"
+
+# 一部のパッケージで-Ouncheckedを使用するように設定します
+export SWIFT_AC_LIBRARY_USES_O_UNCHECKED=true
+
+./${SWIFT_TAR_BALL}/usr/bin/swift build -c release -v > build0.log
+sed -i 's/Hello/Hallo/' Sources/main.swift
+sw./${SWIFT_TAR_BALL}/usr/bin/swiftift build -c release -v > build.log
+grep /bin/ build.log | sed 's/ -v / /g' | tail -n +2 > build.sh
+
+bash build.sh
