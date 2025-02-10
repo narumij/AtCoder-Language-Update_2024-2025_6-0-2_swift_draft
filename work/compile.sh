efdb6c4cde9fb61120f53a416586c9903ba5e75a
narumij <narumij@gmail.com>
2025-02-10 20:44:40 +0900

    script

--- work/compile.sh
+++ work/compile.sh
@@ -1,2 +1,16 @@
 
-cat ./Script/build.sh | bash
+PLATFORM=ubuntu24.04
+LANG_VERSION=6.0.3
+OS_ARCH_SUFFIX="" # arm64等の場合に指定する
+
+SWIFT_VERSION=swift-${LANG_VERSION}-RELEASE
+SWIFT_TAR_BALL="$SWIFT_VERSION-$PLATFORM$OS_ARCH_SUFFIX"
+
+# 一部のパッケージで-Ouncheckedを使用するように設定します
+export SWIFT_AC_LIBRARY_USES_O_UNCHECKED=true
+
+./${SWIFT_TAR_BALL}/usr/bin/swift \
+    build \
+    -c release \
+      --Xswiftc -num-threads --Xswiftc 1 --Xswiftc -j1 \
+    1>&2 |& tee /dev/null
