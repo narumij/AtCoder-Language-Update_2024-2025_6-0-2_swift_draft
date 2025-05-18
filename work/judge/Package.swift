25b217eef0136a6a2d5fb526e5045e1fd55df514
narumij <narumij@gmail.com>
2025-05-18 22:34:53 +0900

    ac-foundation

--- work/judge/Package.swift
+++ work/judge/Package.swift
@@ -42,7 +42,7 @@ let package = Package(
 //      exact: "0.1.8"),
     .package(
       url: "https://github.com/narumij/swift-ac-foundation",
-      exact: "0.1.12"),
+      exact: "0.1.14"),
     .package(
       url: "https://github.com/narumij/swift-ac-collections",
       exact: "0.1.14"),
