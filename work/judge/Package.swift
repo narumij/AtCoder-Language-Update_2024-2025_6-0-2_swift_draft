d39aa4bd9ea71b7b4335b3f276de3036b5bb5e81
narumij <narumij@gmail.com>
2025-05-19 04:04:11 +0900

    package

--- work/judge/Package.swift
+++ work/judge/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "Main",
   
-  // @MainActorとSwift MacrosをmacOSローカルでパッケージを利用する場合に必要な設定値
+  // @MainActorとRegexをmacOSローカルでパッケージを利用する場合に必要な設定値
   platforms: [.macOS(.v13), .iOS(.v16), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
   
   dependencies: [
