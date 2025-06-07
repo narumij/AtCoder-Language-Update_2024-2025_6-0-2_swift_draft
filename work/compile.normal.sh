2e361744215898bb26914487c2992ee28bc42d9c
narumij <narumij@gmail.com>
2025-06-08 00:46:31 +0900

    swifty

--- work/compile.normal.sh
+++ work/compile.normal.sh
@@ -3,9 +3,6 @@
 
 # 環境変数はtoml側で記述
 # これがないとswift-ac-libraryのコンパイルが走ってしまう
-# export SWIFT_AC_LIBRARY_USES_O_UNCHECKED=true
+export SWIFT_AC_LIBRARY_USES_O_UNCHECKED=true
 
-swift \
-  build \
-  -c release
-
+swift build -c release
