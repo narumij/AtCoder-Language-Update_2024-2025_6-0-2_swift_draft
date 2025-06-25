aa883c5f824b20e0f65a0b57e537cfc9f42eb845
narumij <narumij@gmail.com>
2025-06-25 11:44:04 +0900

    exit code

--- work/judge/Sources/main.swift
+++ work/judge/Sources/main.swift
@@ -1,22 +1,48 @@
 import AcFoundation
 
-let (_, M): (Int, Int) = stdin()
-var m: [Pack<Int, Int>: Int] = [:]
-var ans = 0
-for _ in 0 ..< M {
-  var (u,v): (Int, Int) = stdin()
-  if u == v {
-    ans += 1
-    continue
-  }
-  if u > v {
-    swap(&u, &v)
-  }
-  m[.init(u, v), default: 0] += 1
+var (N, m): (Int,Int) = stdin()
+var p_rev = (0..<N) + []
+var p = (0..<N).map { [$0] }
+var e = (0..<N).map { _ in Set<Int>() }
+var u: [Int] = []
+var v: [Int] = []
+for _ in 0..<m {
+  let (_u,_v) = (Int.stdin - 1, Int.stdin - 1)
+  u.append(_u)
+  v.append(_v)
+  e[_u].insert(_v)
+  e[_v].insert(_u)
 }
-m.forEach {
-  ans += $0.value - 1
+let Q = Int.stdin
+for _ in 0..<Q {
+  let x = Int.stdin
+  var vx = p_rev[u[x - 1]]
+  var vy = p_rev[v[x - 1]]
+  if vx != vy {
+    let valx = (e[vx].count) + (p[vx].count)
+    let valy = (e[vy].count) + (p[vy].count)
+    if valx > valy { swap(&vx, &vy) }
+    let sz = p[vx].count
+    for j in 0..<sz {
+      p[vy].append(p[vx][j])
+      p_rev[p[vx][j]] = vy
+    }
+    p[vx].removeAll()
+    for vz in e[vx] {
+      if vz == vy {
+        m -= 1
+        e[vy].remove(vx)
+      } else {
+        if e[vy].contains(vz) {
+          m -= 1
+        } else {
+          e[vy].insert(vz)
+          e[vz].insert(vy)
+        }
+        e[vz].remove(vx)
+      }
+    }
+    e[vx].removeAll()
+  }
+  print(m)
 }
-print(ans)
-
-print(gcd(12, 16))
---
+++ work/judge/tmp/main.3.swift
@@ -0,0 +1,20 @@
+import AcFoundation
+
+let (_, M): (Int, Int) = stdin()
+var m: [Pack<Int, Int>: Int] = [:]
+var ans = 0
+for _ in 0 ..< M {
+  var (u,v): (Int, Int) = stdin()
+  if u == v {
+    ans += 1
+    continue
+  }
+  if u > v {
+    swap(&u, &v)
+  }
+  m[.init(u, v), default: 0] += 1
+}
+m.forEach {
+  ans += $0.value - 1
+}
+print(ans)
