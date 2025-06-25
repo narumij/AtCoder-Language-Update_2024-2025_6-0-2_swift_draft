2f7bcf07c98fe955cbd26a32887d165a1415f450
narumij <narumij@gmail.com>
2025-06-25 13:20:15 +0900

    alt

--- work/compile.alt.sh
+++ work/compile.alt.sh
@@ -6,8 +6,14 @@ SWIFT_PATH="swift-${VERSION}-${PLATFORM}/usr/bin"
 # これがないとswift-ac-libraryのコンパイルが走ってしまう
 export SWIFT_AC_LIBRARY_USES_O_UNCHECKED=true
 
-# ./${SWIFT_PATH}/swift build -c release
 ./${SWIFT_PATH}/swift build --build-system native -c release 1>&2 |& tee /dev/null
 
 # コンパイル所要時間の目安はhello wolrdで5秒程度。
 # それを上回る場合、差分コンパイルに問題が生じている可能性があります。
+
+FILE=".build/release/Main"
+
+if [ ! -f "$FILE" ]; then
+  echo "Error: 初回のビルドに失敗しました: $FILE" >&2
+  exit 1
+fi
