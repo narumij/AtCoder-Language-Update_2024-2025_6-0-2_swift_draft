406a2e617dc4494c5bf4815f75bd0ba1a1196b19
narumij <narumij@gmail.com>
2025-07-25 11:05:40 +0900

    foundation

--- work/install.alt.sh
+++ work/install.alt.sh
@@ -162,7 +162,7 @@ let package = Package(
       url: "https://github.com/narumij/swift-ac-foundation",
       // .unsafeFlags(["-std=c++17"])に対するビルド拒否を迂回するため、revision指定としている
       // branch - main
-      revision: "af842af82bbd2b9d103102e365b210c169e09175"),
+      revision: "8107617fd0083ffd70f22b286d51d03df43e5082"),
     // 平衡二分探索木と順列全列挙です。
     .package(
       url: "https://github.com/narumij/swift-ac-collections",
