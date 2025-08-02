46b659b8bafb73249ac41acf29737b96f6dad567
narumij <narumij@gmail.com>
2025-08-03 02:18:34 +0900

    template

--- template/Sources/main.swift
+++ template/Sources/main.swift
@@ -1,66 +1,31 @@
+import AcCollections
 import AcFoundation
+import AccelerateLinux
+import Algorithms
+import AtCoder
+import BigInt
+import BigNum
+import Bisect
+import CharacterUtil
+import Collections
 import Convenience
+import CxxWrapped
+@preconcurrency import Foundation
+import Foundation
+import IOReader
+import IOUtil
+import Miscellaneous
+import Numerics
+import Pack
+import PermutationModule
 import RedBlackTreeModule
+import UInt8Util
+import simd
 
-let N: Int = .stdin
-let a = RedBlackTreeMultiSet<Int>([Int].stdin(rows: N))
-let (n1, n2, n3) = (a.count(1), a.count(2), a.count(3))
-let helper = IndexHelper3D(N + 2, N + 2, N + 2)
-var dp = [0.0] * helper.count
-for s in 1..<(3 * N + 1) {
-    for j in (N + 1).range {
-        for k in (N + 1).range {
-            let i = s - 2 * j - 3 * k
-            guard (0...N).contains(i) else { continue }
-            let t = i + j + k
-            if t > N {
-                continue
-            }
-            dp[helper[i,j,k]] = Double(N) / Double(t)
-            if i > 0 {
-                dp[helper[i,j,k]] += dp[helper[i - 1,j,k]] * Double(i) / Double(t) }
-            if j > 0 {
-                dp[helper[i,j,k]] += dp[helper[i + 1,j - 1,k]] * Double(j) / Double(t) }
-            if k > 0 {
-                dp[helper[i,j,k]] += dp[helper[i,j + 1,k - 1]] * Double(k) / Double(t) }
-        }
-    }
-}
-print(dp[helper[n1,n2,n3]])
+#if canImport(Glibc)
+  import AccelerateLinux
+#else
+  import Accelerate
+#endif
 
-struct IndexHelper2D {
-  public init(_ width: Int,_ height: Int) {
-    self.width = width
-    self.height = height
-  }
-  let width: Int
-  let height: Int
-  @inlinable
-  var count: Int {
-    return width * height
-  }
-  @inlinable
-  subscript(x: Int, y: Int, z: Int) -> Int {
-    x + y * width
-  }
-}
-
-struct IndexHelper3D {
-  public init(_ width: Int,_ height: Int,_ depth: Int) {
-    self.width = width
-    self.height = height
-    self.depth = depth
-  }
-  let width: Int
-  let height: Int
-  let depth: Int
-  @inlinable
-  var count: Int {
-    return width * height * depth
-  }
-  @inlinable
-  subscript(x: Int, y: Int, z: Int) -> Int {
-    x + y * width + z * width * height
-  }
-}
-
+print("Hello, world!")
