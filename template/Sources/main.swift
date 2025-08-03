58c6595bb335c5b7e286c88763a0bcd875456738
narumij <narumij@gmail.com>
2025-08-03 11:48:06 +0900

    template

--- template/Sources/main.swift
+++ template/Sources/main.swift
@@ -28,4 +28,24 @@ import simd
   import Accelerate
 #endif
 
-print("Hello, world!")
+#if ONLINE_JUDGE
+// 提出した際に実行される
+#else
+// ローカル環境で実行される
+#endif
+
+@MainActor
+public func main() throws {
+  let N: Int = stdin()
+  //  let (N, Q): (Int, Int) = stdin()
+  //  let (N, M) = (Int.stdin, Int.stdin)
+  //  let (H, W) = (Int.stdin, Int.stdin)
+  //  let A = [Int].stdin(columns: N)
+  //  let S = [Character].stdin
+  //  let S = [Character].stdin(columns: N)
+  //  let S = [[Character]].stdin(rows: H, columns: W)
+  
+  print("Hello, world! (\(N))")
+}
+
+try main()
