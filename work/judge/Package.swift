2bfc24fe924e31b1f91948071961c462b2a68b66
narumij <narumij@gmail.com>
2025-08-31 18:40:45 +0900

    memoize

--- work/judge/Package.swift
+++ work/judge/Package.swift
@@ -54,7 +54,7 @@ let package = Package(
     // メモ化マクロです。
     .package(
       url: "https://github.com/narumij/swift-ac-memoize",
-      exact: "0.1.6"),
+      branch: "main"),
   ],
   targets: [
     .executableTarget(
